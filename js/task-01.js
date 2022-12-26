3e40c5e57f30640d43f5dc8a28331321bcd20ce4
Yaroslav Molodichenko <cuda.jaro@gmail.com>
2022-12-26 20:00:03 +0200

    third task

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,10 @@
 const list = document.querySelectorAll('.item')
-console.log('Кількість категорій: ${list.length}')
+console.log(`Кількість категорій: ${list.length}`)
 
 const ul = document.querySelectorAll('#categories>li')
 ul.forEach((el) => {
     console.log(
-        'Категорія: ${el.firstElementChild.textContent}, Кількість елементів: ${el.lastElementChild.length}'
+        `Категорія: ${el.firstElementChild.textContent}, 
+        Кількість елементів: ${el.lastElementChild.children.length}`
     )
 })
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
+]
+
+const ulEL = document.querySelector('#ingredients')
+
+const list = ingredients.reduce((str, item) => str + `<li>${item}<li>`, '')
+
+ulEL.innerHTML = list
