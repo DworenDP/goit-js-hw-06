bfcaa1e8adecf660b501c94e5a4b98e5fcc5f0b2
Yaroslav Molodichenko <cuda.jaro@gmail.com>
2022-12-26 20:56:12 +0200

    add comment

--- js/task-02.js
+++ js/task-02.js
@@ -7,8 +7,12 @@ const ingredients = [
     'Condiments',
 ]
 
+// створюю пустий масив списку ulEL з id="ingredients"
 const ulEL = document.querySelector('#ingredients')
 
+// за допомогою методу reduce розбираю масив ingredients на елементи та укладаю
+// кожен елемент масиву за допомогою шаблонної строки в елементи списку
 const list = ingredients.reduce((str, item) => str + `<li>${item}<li>`, '')
 
+// за допомогою властивості innerHTML додаю список елементів в DOM-дерево
 ulEL.innerHTML = list
