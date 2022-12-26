5d1e351fe01dc2652af2d4fd2c3724d86d6689b5
Yaroslav Molodichenko <cuda.jaro@gmail.com>
2022-12-26 18:54:37 +0200

    first task

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const list = document.querySelectorAll('.item')
+console.log('Кількість категорій: ${list.length}')
+
+const ul = document.querySelectorAll('#categories>li')
+ul.forEach((el) => {
+    console.log(
+        'Категорія: ${el.firstElementChild.textContent}, Кількість елементів: ${el.lastElementChild.length}'
+    )
+})
