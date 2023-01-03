69e605a2f9b263978c8ccad1b48c2cd12bde6c80
Yaroslav Molodichenko <cuda.jaro@gmail.com>
2023-01-03 20:24:02 +0200

    10 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,24 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}
+
+const bodyEl = document.querySelector('body')
+
+const color = getRandomHexColor()
+
+const buttonColor = document.querySelector('.change-color')
+console.log(buttonColor)
+buttonColor.addEventListener('click', changecolor)
+
+const spanColor = document.querySelector('.color')
+console.log(spanColor)
+spanColor.textContent = color
+
+bodyEl.style.backgroundColor = color
+
+function changecolor() {
+    bodyEl.style.backgroundColor = color
+    spanColor.textContent = color
+    const color = getRandomHexColor()
+    bodyEl.style.backgroundColor = getRandomHexColor()
 }
