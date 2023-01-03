4f58617593f84532ea37939a6e0f4802dc381f37
Yaroslav Molodichenko <cuda.jaro@gmail.com>
2023-01-03 08:34:28 +0200

    edit the fourth task

--- js/task-04.js
+++ js/task-04.js
@@ -5,13 +5,13 @@ let counterValue = document.querySelector('#value')
 const dekrBtn = document.querySelector('#counter').firstElementChild
 const inkrBtn = document.querySelector('#counter').lastElementChild
 
-inkrBtn.addEventListener('click', (event) => {
+inkrBtn.addEventListener('click', () => {
     valueEl += 1
 
     counterValue.innerText = valueEl
 })
 
-dekrBtn.addEventListener('click', (event) => {
+dekrBtn.addEventListener('click', () => {
     valueEl -= 1
 
     counterValue.innerText = valueEl
